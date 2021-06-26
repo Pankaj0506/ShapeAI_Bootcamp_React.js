8efcafe885aa988e244b412cd730c0b3a21bd8f4
Pankaj Lahoti <84628584+Pankaj0506@users.noreply.github.com>
2021-06-26 19:15:16 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Bootcamp</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. At nulla
+        debitis animi reiciendis adipisci expedita totam officia assumenda illum
+        quis! Quo deserunt aspernatur voluptate eius atque modi ab saepe
+        praesentium?
+      </p>
+    </div>
+  );
+}
+
+export default Info;
